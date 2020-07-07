f0f71841f57313426d1673f03bcf773449a2365a
xescugc <xescugil@gmail.com>
2020-07-07 12:40:18 +0200

    errcode: List of all the errros

---
+++ errcode/code.go
@@ -0,0 +1,26 @@
+package errcode
+
+import "errors"
+
+var (
+	ErrGraphEdgeReplace              = errors.New("graph edge replace source not found")
+	ErrGraphRequiredEdgeID           = errors.New("graph edge ID is required")
+	ErrGraphRequiredEdgeTarget       = errors.New("graph edge target is required")
+	ErrGraphRequiredEdgeSource       = errors.New("graph edge source is required")
+	ErrGraphNotFoundEdgeTarget       = errors.New("graph edge target not found")
+	ErrGraphNotFoundEdgeSource       = errors.New("graph edge source not found")
+	ErrGraphAlreadyExistsEdge        = errors.New("graph edge already exists")
+	ErrGraphAlreadyExistsEdgeID      = errors.New("graph edge ID already exists")
+	ErrGraphRequiredNodeCanonical    = errors.New("graph node canonical is required")
+	ErrGraphRequiredNodeID           = errors.New("graph node ID is required")
+	ErrGraphAlreadyExistsNode        = errors.New("graph node already exists")
+	ErrGraphAlreadyExistsNodeID      = errors.New("graph node ID already exists")
+	ErrGraphNotFoundNode             = errors.New("graph node not found")
+	ErrGraphRequiredEdgeBetweenNodes = errors.New("graph requires edge between nodes")
+
+	ErrProviderNotFoundResource   = errors.New("provider resource not found")
+	ErrProviderNotFoundDataSource = errors.New("provider data source not found")
+	ErrProviderNotFound           = errors.New("provider not found")
+
+	ErrInvalidTFStateFile = errors.New("invalid Terraform State file")
+)
