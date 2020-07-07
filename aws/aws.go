7ce320357021769a6c4ea88768e323973889471c
xescugc <xescugil@gmail.com>
2020-07-07 12:43:01 +0200

    aws: Provider implementation of AWS

---
+++ aws/aws.go
@@ -0,0 +1,106 @@
+package aws
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/cycloidio/infraview/errcode"
+	"github.com/cycloidio/infraview/provider"
+
+	tfdocAWS "github.com/cycloidio/tfdocs/providers/aws"
+	"github.com/cycloidio/tfdocs/resource"
+)
+
+// Provider is the Type that defines the interface 'provider.Interface'
+type Provider struct {
+	provider.NopProvider
+}
+
+// Type returns the type of the implementation
+func (a Provider) Type() provider.Type { return provider.AWS }
+
+// IsNode returns if the resources is a Node
+func (a Provider) IsNode(resource string) bool {
+	_, ok := nodeTypes[resource]
+	return ok
+}
+
+// IsEdge returns if the resource is an Edge
+func (a Provider) IsEdge(resource string) bool {
+	_, ok := edgeTypes[resource]
+	return ok
+}
+
+// Resource returns the resource information
+func (a Provider) Resource(resource string) (*resource.Resource, error) {
+	r, err := tfdocAWS.GetResource(resource)
+	if err != nil {
+		return nil, fmt.Errorf("could not get resource %q: %w", resource, errcode.ErrProviderNotFoundResource)
+	}
+	return r, nil
+}
+
+// DataSource returns the resource information
+func (a Provider) DataSource(resource string) (*resource.Resource, error) {
+	r, err := tfdocAWS.GetDataSource(resource)
+	if err != nil {
+		return nil, fmt.Errorf("could not get resource %q: %w", resource, errcode.ErrProviderNotFoundDataSource)
+	}
+	return r, nil
+}
+
+// These 2 regexp match keys like 'ingress.2988878453.security_groups.2496918042' and
+// are used for TF 0.11 as the State is flat
+var reSGEgress = regexp.MustCompile(`^egress\.\d+\.security_groups\.\d+$`)
+var reSGIngress = regexp.MustCompile(`^ingress\.\d+\.security_groups\.\d+$`)
+
+// ResourceInOut returns the In and Out of the rs based on the cfg
+func (a Provider) ResourceInOut(rs string, cfg map[string]interface{}) ([]string, []string) {
+	var ins, outs []string
+	if rs == "aws_security_group" {
+		ingress, inok := cfg["ingress"]
+		if inok {
+			for _, in := range ingress.([]interface{}) {
+				if sgs, ok := in.(map[string]interface{})["security_groups"].([]interface{}); ok {
+					for _, sg := range sgs {
+						ins = append(ins, sg.(string))
+					}
+				}
+			}
+		}
+
+		egress, egok := cfg["egress"]
+		if egok {
+			for _, eg := range egress.([]interface{}) {
+				if sgs, ok := eg.(map[string]interface{})["security_groups"].([]interface{}); ok {
+					for _, sg := range sgs {
+						outs = append(outs, sg.(string))
+					}
+				}
+			}
+		}
+
+		// This means the security group config is flat, so it
+		// needs to be iterated and the keys parsed with regex
+		if !inok && !egok {
+			for k, v := range cfg {
+				if reSGIngress.MatchString(k) {
+					ins = append(ins, v.(string))
+				} else if reSGEgress.MatchString(k) {
+					outs = append(outs, v.(string))
+				}
+			}
+		}
+	} else if rs == "aws_security_group_rule" {
+		in, ok := cfg["source_security_group_id"]
+		if ok && in != nil {
+			ins = append(ins, in.(string))
+		}
+
+		out, ok := cfg["security_group_id"]
+		if ok && in != nil {
+			outs = append(outs, out.(string))
+		}
+	}
+	return ins, outs
+}
---
+++ aws/graph.go
@@ -0,0 +1,64 @@
+package aws
+
+var (
+	// nodeTypes all the Nodes that we support right now
+	nodeTypes = map[string]struct{}{
+		"aws_api_gateway_resource":          struct{}{},
+		"aws_athena_database":               struct{}{},
+		"aws_autoscaling_group":             struct{}{},
+		"aws_dax_cluster":                   struct{}{},
+		"aws_db_instance":                   struct{}{},
+		"aws_directory_service_directory":   struct{}{},
+		"aws_dms_replication_instance":      struct{}{},
+		"aws_dx_gateway":                    struct{}{},
+		"aws_dynamodb_table":                struct{}{},
+		"aws_ebs_volume":                    struct{}{},
+		"aws_ecs_cluster":                   struct{}{},
+		"aws_ecs_service":                   struct{}{},
+		"aws_efs_file_system":               struct{}{},
+		"aws_eip":                           struct{}{},
+		"aws_eks_cluster":                   struct{}{},
+		"aws_elasticache_cluster":           struct{}{},
+		"aws_elastic_beanstalk_application": struct{}{},
+		"aws_elasticsearch_domain":          struct{}{},
+		"aws_elb":                           struct{}{},
+		"aws_emr_cluster":                   struct{}{},
+		"aws_iam_user":                      struct{}{},
+		"aws_instance":                      struct{}{},
+		"aws_internet_gateway":              struct{}{},
+		"aws_kinesis_stream":                struct{}{},
+		"aws_lambda_function":               struct{}{},
+		"aws_lightsail_instance":            struct{}{},
+		"aws_mq_broker":                     struct{}{},
+		"aws_media_store_container":         struct{}{},
+		"aws_nat_gateway":                   struct{}{},
+		"aws_rds_cluster":                   struct{}{},
+		"aws_rds_cluster_instance":          struct{}{},
+		"aws_redshift_cluster":              struct{}{},
+		"aws_s3_bucket":                     struct{}{},
+		"aws_storagegateway_gateway":        struct{}{},
+		"aws_sqs_queue":                     struct{}{},
+		"aws_vpn_gateway":                   struct{}{},
+		"aws_batch_job_definition":          struct{}{},
+		"aws_neptune_cluster":               struct{}{},
+		"aws_alb":                           struct{}{},
+		"aws_lb":                            struct{}{},
+		"aws_cloudfront_distribution":       struct{}{},
+		"aws_elasticache_replication_group": struct{}{},
+		"aws_launch_template":               struct{}{},
+	}
+
+	// noSecurityGroup is a map of all the resourcese that do not
+	// have/relay on SecurityGroups
+	noSecurityGroup = map[string]struct{}{
+		"aws_s3_bucket":               struct{}{},
+		"aws_cloudfront_distribution": struct{}{},
+	}
+
+	// edgeTypes map of all the supported Edges
+	edgeTypes = map[string]struct{}{
+		"aws_security_group":      struct{}{},
+		"aws_security_group_rule": struct{}{},
+		//"aws_cloudfront_distribution": struct{}{},
+	}
+)
